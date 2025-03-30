d6306190c15b77c41d394630f804c093c05ece71
Jwalant Patel <142465606+Jawlt@users.noreply.github.com>
2025-03-29 20:46:50 -0400

    update

--- Assets/Scripts/inventory/InventorySystem.cs
+++ Assets/Scripts/inventory/InventorySystem.cs
@@ -47,7 +47,7 @@ public class InventorySystem : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             ToggleInventory();
         }
